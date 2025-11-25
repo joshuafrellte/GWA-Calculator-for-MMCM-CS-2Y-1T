0ba65e5d3a1ce3c4a4c139452ba7d56e66eda66b
unknown <joshuafrellt@gmail.com>
2025-11-25 14:04:15 +0800

    started grade calculation logic

--- script.js
+++ script.js
@@ -6,7 +6,72 @@ inputs.forEach(input => {
     input.step = '0.25';
 });
 
+const finalGrade = document.getElementById('finalGrade');
+const calculateBtn = document.getElementById('calculateBtn');
 
+const oneUnitCourses = ['CS106L','IT102L'];
+const twoUnitCourses = ['CS106','IT102','PE014'];
+const threeUnitCourses = ['ENV075','HUM021','MATH035','CS121'];
+const allCourses = [...oneUnitCourses, ...twoUnitCourses, ...threeUnitCourses];
+console.log(allCourses);
+
+const oneUnitCourseTotalUnits = oneUnitCourses.length * 1;
+const twoUnitCourseTotalUnits = twoUnitCourses.length * 2;
+const threeUnitCourseTotalUnits = threeUnitCourses.length * 3;
+const TOTAL_UNITS = oneUnitCourseTotalUnits + twoUnitCourseTotalUnits + threeUnitCourseTotalUnits;
+console.log(TOTAL_UNITS);
+
+const gradeSum = getGradeSum();
+
+const CS106 = document.getElementById('CS106');
+const CS106L = document.getElementById('CS106L');
+const ENV075 = document.getElementById('ENV075');
+const HUM021 = document.getElementById('HUM021');
+const MATH035 = document.getElementById('MATH035');
+const IT102 = document.getElementById('IT102');
+const IT102L = document.getElementById('IT102L');
+const PE014 = document.getElementById('PE014');
+const CS121 = document.getElementById('CS121');
+
+const CS106_GRADE = CS106.value;
+const CS106L_GRADE = CS106L.value;
+const ENV075_GRADE = ENV075.value;
+const HUM021_GRADE = HUM021.value;
+const MATH035_GRADE = MATH035.value;
+const IT102_GRADE = IT102.value;
+const IT102L_GRADE = IT102L.value;
+const PE014_GRADE = PE014.value;
+const CS121_GRADE = CS121.value;
+
+calculateBtn.onclick = function() {
+    const CS106_GRADE = CS106.value;
+    const CS106L_GRADE = CS106L.value;
+    const ENV075_GRADE = ENV075.value;
+    const HUM021_GRADE = HUM021.value;
+    const MATH035_GRADE = MATH035.value;
+    const IT102_GRADE = IT102.value;
+    const IT102L_GRADE = IT102L.value;
+    const PE014_GRADE = PE014.value;
+    const CS121_GRADE = CS121.value;
+    console.log(CS106_GRADE);
+}
+
+allCourses.forEach(getGradeSum);
+function getGradeSum(course) {
+    
+}
+
+function isOneUnit(course) {
+    return oneUnitCourses.includes(course);
+}
+
+function isTwoUnit(course) {
+    return twoUnitCourses.includes(course);
+}
+
+function isThreeUnit(course) {
+    return threeUnitCourses.includes(course);
+}
 
 
 // create array of stanines
